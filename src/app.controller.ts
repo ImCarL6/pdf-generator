502260cb4bd83bbbb4c022df8fdef852762793d2
Carlos Aparecido <90780691+ImCarL6@users.noreply.github.com>
2023-08-25 17:28:41 -0300

    test in prod

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Get } from '@nestjs/common';
+// app.controller.ts
+import { Controller, Post, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
+import * as path from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('generate-pdf')
+  async generatePDF(@Res() res: Response): Promise<void> {
+    const pdf = await this.appService.generatePDF();
+
+    const pdfFilePath = path.join(__dirname, 'resume.pdf');
+
+    // Set appropriate headers for the PDF response
+    res.setHeader('Content-Type', 'application/pdf');
+    res.setHeader('Content-Disposition', 'attachment; filename=example.pdf');
+
+    // Send the PDF content as the response
+    res.sendFile(pdfFilePath);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,69 @@
 import { Injectable } from '@nestjs/common';
+import * as puppeteer from 'puppeteer';
+import jsPDF from 'jspdf';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async generatePDF(): Promise<jsPDF> {
+
+    const browser = await puppeteer.launch({defaultViewport: {width: 1920, height: 1080}, headless: 'new'});
+    const page = await browser.newPage();
+
+    await page.goto('https://carlos-silva-resume.vercel.app/');
+    
+    await page.waitForSelector('#bd-container');
+
+    await page.waitForSelector('.home__img');
+
+    await page.click('#snow-button')
+
+    await page.evaluate(() => {
+      const elementsToRemove = document.querySelectorAll('.language-toggle-container'); // Replace with your selector
+      elementsToRemove.forEach(element => element.remove());
+    });
+
+    await page.evaluate(() => {
+      const divToRemove = document.getElementById('tsparticles');
+      if (divToRemove) {
+        divToRemove.remove();
+      }
+    });
+
+    await page.evaluate(() => {
+      const elementsToRemove = document.querySelectorAll('#theme-button'); // Replace with your selector
+      elementsToRemove.forEach(element => element.remove());
+    });
+
+    await page.evaluate(() => {
+      const elementsToRemove = document.querySelectorAll('#snow-button'); // Replace with your selector
+      elementsToRemove.forEach(element => element.remove());
+    });
+
+    await page.evaluate(() => {
+      const elementsToRemove = document.querySelectorAll('#resume__generate'); // Replace with your selector
+      elementsToRemove.forEach(element => element.remove());
+    });
+
+    await page.click('#home > div.home__container.section.bd-grid > div.home__data.bd-grid > img')
+
+    await page.evaluate(() => {
+      return new Promise<void>((resolve) => {
+        setTimeout(() => {
+          resolve();
+        }, 5000);
+      });
+    });
+    
+    const element = await page.$('#area-cv')
+
+    const pdf: Buffer | string = await element.screenshot({ omitBackground: true})
+
+    const pdfFile: jsPDF = new jsPDF({format: [417, 255]});
+    pdfFile.addImage(pdf, 'PNG', 0, 0, 0, 0);
+    pdfFile.save('./dist/resume.pdf')
+  
+    await browser.close();
+
+    return pdfFile;
   }
 }
