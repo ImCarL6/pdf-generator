7989e7fff0b4c62d3c64da1cc731aeaa73140250
Carlos Aparecido <90780691+ImCarL6@users.noreply.github.com>
2023-08-26 21:05:48 -0300

    lint fix

--- src/app.service.ts
+++ src/app.service.ts
@@ -2,14 +2,14 @@ import { Injectable } from '@nestjs/common';
 import * as puppeteer from 'puppeteer-core';
 import jsPDF from 'jspdf';
 import * as AWS from 'aws-sdk';
-import { v4 as uuidv4 } from 'uuid'
+import { v4 as uuidv4 } from 'uuid';
 
 @Injectable()
 export class AppService {
   async generatePDF(): Promise<string> {
     const s3 = new AWS.S3({
       accessKeyId: process.env.AWS_KEY,
-      secretAccessKey: process.env.AWS_SECRET
+      secretAccessKey: process.env.AWS_SECRET,
     });
 
     const browserlessKey: string = process.env.BROWSERLESS_KEY;
@@ -42,9 +42,9 @@ export class AppService {
         divToRemove.remove();
       }
     });
-    
+
     await page.evaluate(() => {
-      const elementsToRemove = document.querySelectorAll('#resume__generate'); // Replace with your selector
+      const elementsToRemove = document.querySelectorAll('#resume__generate');
       elementsToRemove.forEach((element) => element.remove());
     });
 
@@ -55,18 +55,18 @@ export class AppService {
     const pdfFile: jsPDF = new jsPDF({ format: [405, 240] });
     pdfFile.addImage(pdf, 'PNG', 0, 0, 0, 0);
 
-    const pdfS3 = Buffer.from(pdfFile.output('arraybuffer')) 
+    const pdfS3 = Buffer.from(pdfFile.output('arraybuffer'));
 
     await browser.close();
 
-    const fileName: string = uuidv4()
+    const fileName: string = uuidv4();
 
     await s3
       .putObject({
         Bucket: process.env.AWS_BUCKET,
         Key: fileName,
         Body: pdfS3,
-        ContentType: 'application/pdf'
+        ContentType: 'application/pdf',
       })
       .promise();
 
